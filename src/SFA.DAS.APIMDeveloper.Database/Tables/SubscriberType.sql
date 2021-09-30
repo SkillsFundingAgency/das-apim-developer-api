49cf761fbacf9cec19c6bcd489787ef3667a7c79
Ewan Noble <EwanNoble@users.noreply.github.com>
2021-09-30 11:37:05 +0100

    Fix foreign keys

--- src/SFA.DAS.APIMDeveloper.Database/Tables/SubscriberType.sql
+++ src/SFA.DAS.APIMDeveloper.Database/Tables/SubscriberType.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [apim_dev].[SubscriberType]
 (
-    [Id] INT NOT NULL PRIMARY KEY,
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Name] NVARCHAR(50) NOT NULL
 )
--- src/SFA.DAS.APIMDeveloper.Database/Tables/SubscriptionAudit.sql
+++ src/SFA.DAS.APIMDeveloper.Database/Tables/SubscriptionAudit.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [apim_dev].[SubscriptionAudit]
 (
-    [SubscriptionId] INT NULL PRIMARY KEY FOREIGN KEY REFERENCES Subscription(Id),
+    [SubscriptionId] INT NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES [apim_dev].[Subscription](Id),
     [UserRef] NVARCHAR(50) NOT NULL,
     [Action] NVARCHAR(50) NOT NULL
 )

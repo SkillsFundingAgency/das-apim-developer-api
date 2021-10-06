d7c88ebb62de659786c9ca99b26aaed3e881202b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-10-06 22:01:00 +0100

    FAI-87 - Add controller test for create subscription

---
+++ src/SFA.DAS.Apim.Developer.Api.UnitTests/Controllers/Subscription/WhenCreatingASubscription.cs
@@ -0,0 +1,82 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Api.ApiRequests;
+using SFA.DAS.Apim.Developer.Api.Controllers;
+using SFA.DAS.Apim.Developer.Application.AzureApimManagement.Commands.CreateUserSubscription;
+using SFA.DAS.Testing.AutoFixture;
+using ValidationResult = SFA.DAS.Apim.Developer.Domain.Validation.ValidationResult;
+
+namespace SFA.DAS.Apim.Developer.Api.UnitTests.Controllers.Subscription
+{
+    public class WhenCreatingASubscription
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Request_Is_Sent_To_The_Mediator_Handler(
+            Guid id,
+            CreateSubscriptionApiRequest request,
+            CreateUserSubscriptionCommandResponse mediatorResponse,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] SubscriptionController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.Is<CreateUserSubscriptionCommand>(c =>
+                        c.ProductName.Equals(request.ProductId)
+                        && c.InternalUserId.Equals(request.AccountIdentifier)),
+                    It.IsAny<CancellationToken>())).ReturnsAsync(mediatorResponse);
+
+            var controllerResult = await controller.CreateSubscription(id, request) as CreatedResult;
+
+            controllerResult!.StatusCode.Should().Be((int) HttpStatusCode.Created);
+            controllerResult.Value.Should().BeEquivalentTo(new{Id=mediatorResponse.SubscriptionId});
+        }
+        
+        [Test, MoqAutoData]
+        public async Task And_Validation_Exception_Then_Returns_BadRequest(
+            Guid id,
+            string errorKey,
+            CreateSubscriptionApiRequest request,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] SubscriptionController controller)
+        {
+            var validationResult = new ValidationResult{ValidationDictionary = { {errorKey,"Some error"}}};
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.IsAny<CreateUserSubscriptionCommand>(),
+                    It.IsAny<CancellationToken>()))
+                .Throws(new ValidationException(validationResult.DataAnnotationResult, null, null));
+
+            var controllerResult = await controller.CreateSubscription(id, request) as ObjectResult;
+
+            controllerResult!.StatusCode.Should().Be((int)HttpStatusCode.BadRequest);
+            controllerResult.Value.ToString().Should().Contain(errorKey);
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task And_Exception_Then_Returns_Error(
+            Guid id,
+            CreateSubscriptionApiRequest request,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] SubscriptionController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.IsAny<CreateUserSubscriptionCommand>(),
+                    It.IsAny<CancellationToken>()))
+                .Throws<InvalidOperationException>();
+
+            var controllerResult = await controller.CreateSubscription(id, request) as StatusCodeResult;
+
+            controllerResult!.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Apim.Developer.Api.ApiRequests;
+using SFA.DAS.Apim.Developer.Application.AzureApimManagement.Commands.CreateUserSubscription;
+
+namespace SFA.DAS.Apim.Developer.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<SubscriptionController> _logger;
+
+        public SubscriptionController (IMediator mediator, ILogger<SubscriptionController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<IActionResult> CreateSubscription([FromRoute]Guid id, [FromBody]CreateSubscriptionApiRequest request)
+        {
+            try
+            {
+                var queryResult = await _mediator.Send(new CreateUserSubscriptionCommand
+                {
+                    ProductName = request.ProductId,
+                    InternalUserId = request.AccountIdentifier
+                });
+
+                return Created("", new {Id = queryResult.SubscriptionId});
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(e.ValidationResult.ErrorMessage);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                return StatusCode((int) HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}

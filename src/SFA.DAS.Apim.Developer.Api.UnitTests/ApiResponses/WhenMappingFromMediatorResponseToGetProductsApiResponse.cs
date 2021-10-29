6495b3d9f80feb59028c5e75b3b4de6ee7e8510c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-10-29 20:09:37 +0100

    FAI-84 - Add mapping from Model to ApiResponse

---
+++ src/SFA.DAS.Apim.Developer.Api.UnitTests/ApiResponses/WhenMappingFromMediatorResponseToGetProductsApiResponse.cs
@@ -0,0 +1,19 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Api.ApiResponses;
+using SFA.DAS.Apim.Developer.Application.AzureApimManagement.Queries;
+
+namespace SFA.DAS.Apim.Developer.Api.UnitTests.ApiResponses
+{
+    public class WhenMappingFromMediatorResponseToGetProductsApiResponse
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(GetProductsQueryResponse source)
+        {
+            var actual = (GetProductsApiResponse)source;
+            
+            actual.Products.Should().BeEquivalentTo(source.Products);
+        }
+    }
+}

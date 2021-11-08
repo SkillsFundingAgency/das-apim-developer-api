db678bf043cce699afd32ffd9bb0f61a708faaa6
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-08 20:08:02 +0000

    FAI-130 - Add handler for getting user subscriptions

---
+++ src/SFA.DAS.Apim.Developer.Application.UnitTests/AzureApimManagement/Queries/WhenHandlingGetUserSubscriptionsQuery.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Application.AzureApimManagement.Queries.GetUserSubscriptions;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
+using SFA.DAS.Apim.Developer.Domain.Models;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Apim.Developer.Application.UnitTests.AzureApimManagement.Queries
+{
+    public class WhenHandlingGetUserSubscriptionsQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Service_Is_Called_And_Data_Returned_For_Employer(
+            GetUserSubscriptionsQuery query,
+            List<UserSubscription> serviceResponse,
+            [Frozen] Mock<ISubscriptionService> service,
+            GetUserSubscriptionsQueryHandler handler)
+        {
+            service.Setup(x => x.GetUserSubscriptions(query.InternalUserId, ApimUserType.Employer)).ReturnsAsync(serviceResponse);
+
+            var actual = await handler.Handle(query, CancellationToken.None);
+
+            actual.UserSubscriptions.Should().BeEquivalentTo(serviceResponse);
+        }
+        
+        [Test, MoqAutoData]
+        public async Task Then_The_Service_Is_Called_And_Data_Returned_For_Provider(
+            GetUserSubscriptionsQuery query,
+            int providerId,
+            List<UserSubscription> serviceResponse,
+            [Frozen] Mock<ISubscriptionService> service,
+            GetUserSubscriptionsQueryHandler handler)
+        {
+            query.InternalUserId = providerId.ToString();
+            service.Setup(x => x.GetUserSubscriptions(query.InternalUserId, ApimUserType.Provider)).ReturnsAsync(serviceResponse);
+
+            var actual = await handler.Handle(query, CancellationToken.None);
+
+            actual.UserSubscriptions.Should().BeEquivalentTo(serviceResponse);
+        }
+    }
+}

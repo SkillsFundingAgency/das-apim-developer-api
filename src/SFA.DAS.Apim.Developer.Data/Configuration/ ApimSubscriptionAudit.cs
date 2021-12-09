32ba5e78403b97874d3f239613f5b8711f30e221
A-J-Wright <alissa.wright@education.gov.uk>
2021-12-09 13:55:07 +0000

    change action max value

--- src/SFA.DAS.Apim.Developer.Data/Configuration/ ApimSubscriptionAudit.cs
+++ src/SFA.DAS.Apim.Developer.Data/Configuration/ ApimSubscriptionAudit.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace SFA.DAS.Apim.Developer.Data.Configuration
@@ -12,8 +13,8 @@ namespace SFA.DAS.Apim.Developer.Data.Configuration
 
             builder.Property(x => x.UserId).HasColumnName("UserId").HasColumnType("uniqueidentifier").IsRequired();
             builder.Property(x => x.ProductName).HasColumnName("ProductName").HasColumnType("varchar").HasMaxLength(250).IsRequired();
-            builder.Property(x => x.Action).HasColumnName("Action").HasColumnType("varchar").HasMaxLength(50).IsRequired();
-            builder.Property(x => x.Timestamp).HasColumnName("Timestamp").HasColumnType("datetime").IsRequired();
+            builder.Property(x => x.Action).HasColumnName("Action").HasColumnType("varchar").IsRequired();
+            builder.Property(x => x.Timestamp).HasColumnName("Timestamp").HasColumnType("datetime").IsRequired().ValueGeneratedOnAdd();
 
             builder.HasIndex(x => x.Id).IsUnique();
         }
--- src/SFA.DAS.Apim.Developer.Database/Tables/ApimSubscriptionAudit.sql
+++ src/SFA.DAS.Apim.Developer.Database/Tables/ApimSubscriptionAudit.sql
@@ -3,6 +3,6 @@ CREATE TABLE [dbo].[ApimSubscriptionAudit]
     [Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
     [UserId] varchar(250) NOT NULL,
     [ProductName] VARCHAR(250) NOT NULL,
-    [Action] VARCHAR(50) NOT NULL,
+    [Action] VARCHAR(MAX) NOT NULL,
     [Timestamp] DATETIME NOT NULL
 )

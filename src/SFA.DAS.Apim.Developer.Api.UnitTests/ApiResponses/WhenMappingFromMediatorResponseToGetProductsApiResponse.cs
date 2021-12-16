555bb51f8ab0ec94bba85a9fe908d7aa010ea4f3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-12-16 08:03:42 +0000

    FAI-194 - Reformat API spec adding version and removing headers
    Removed headers that can not be set by the user
    Added missing X-Version header

--- src/SFA.DAS.Apim.Developer.Api.UnitTests/ApiResponses/WhenMappingFromMediatorResponseToGetProductsApiResponse.cs
+++ src/SFA.DAS.Apim.Developer.Api.UnitTests/ApiResponses/WhenMappingFromMediatorResponseToGetProductsApiResponse.cs
@@ -1,38 +1,202 @@
+using System.Collections.Generic;
 using System.Linq;
 using AutoFixture.NUnit3;
 using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using SFA.DAS.Apim.Developer.Api.ApiResponses;
 using SFA.DAS.Apim.Developer.Application.AzureApimManagement.Queries.GetProducts;
+using SFA.DAS.Apim.Developer.Domain.Models;
 
 namespace SFA.DAS.Apim.Developer.Api.UnitTests.ApiResponses
 {
     public class WhenMappingFromMediatorResponseToGetProductsApiResponse
     {
         [Test, AutoData]
-        public void Then_The_Fields_Are_Mapped(GetProductsQueryResponse source)
+        public void Then_The_Fields_Are_Mapped(GetProductsQueryResponse source, JObject documentation)
         {
+            foreach (var sourceProduct in source.Products)
+            {
+                sourceProduct.Documentation = JsonConvert.SerializeObject(documentation);
+            }
             var actual = (GetProductsApiResponse)source;
             
             actual.Products.Should().BeEquivalentTo(source.Products, options => 
-                options.Excluding(product => product.DisplayName));
+                options.Excluding(product => product.DisplayName).Excluding(product => product.Documentation));
             actual.Products.Select(item => item.DisplayName).Should().BeEquivalentTo(
                 source.Products.Select(product => product.DisplayName + " (Live)"));
         }
         
         [Test, AutoData]
-        public void Then_Adds_Sandbox_To_DisplayName_If_In_Id(GetProductsQueryResponse source)
+        public void Then_Adds_Sandbox_To_DisplayName_If_In_Id(GetProductsQueryResponse source, JObject documentation)
         {
             foreach (var product in source.Products)
             {
                 product.Id = product + "-Sandbox";
+                product.Documentation = JsonConvert.SerializeObject(documentation);
             }
             var actual = (GetProductsApiResponse)source;
             
             actual.Products.Should().BeEquivalentTo(source.Products, options => 
-                options.Excluding(product => product.DisplayName));
+                options.Excluding(product => product.DisplayName).Excluding(product => product.Documentation));
             actual.Products.Select(item => item.DisplayName).Should().BeEquivalentTo(
                 source.Products.Select(product => product.DisplayName + " (Sandbox)"));
         }
+        
+        [Test, AutoData]
+        public void Then_The_Non_Required_Security_Query_Options_Is_Removed_From_The_Documentation(Product product)
+        {
+            product.Documentation = testDocumentation;
+            var source = new GetProductsQueryResponse
+            {
+                Products = new List<Product> { product }
+            };
+            
+            var actual = (GetProductsApiResponse)source;
+
+            var actualObject = JObject.Parse(actual.Products.First().Documentation);
+            Assert.IsNotNull(actualObject);
+            actualObject["security"]?.Count(c => c["apiKeyQuery"] != null).Should().Be(0);
+        }
+        
+        [Test, AutoData]
+        public void Then_The_Non_Required_Headers_Are_Removed_From_The_Documentation(Product product)
+        {
+            product.Documentation = testDocumentation;
+            var source = new GetProductsQueryResponse
+            {
+                Products = new List<Product> { product }
+            };
+            
+            var actual = (GetProductsApiResponse)source;
+
+            var actualObject = JObject.Parse(actual.Products.First().Documentation);
+            Assert.IsNotNull(actualObject);
+            actualObject["paths"].First().First().First().Children()["parameters"].Values()
+                .Any(c => c["name"].Value<string>() == "x-request-context-subscription-name").Should().BeFalse();
+            actualObject["paths"].First().First().First().Children()["parameters"].Values()
+                .Any(c => c["name"].Value<string>() == "x-request-context-subscription-is-sandbox").Should().BeFalse();
+        }
+        
+        [Test, AutoData]
+        public void Then_The_Version_Header_Is_Added_To_The_Documentation(Product product)
+        {
+            product.Documentation = testDocumentation;
+            var source = new GetProductsQueryResponse
+            {
+                Products = new List<Product> { product }
+            };
+            
+            var actual = (GetProductsApiResponse)source;
+
+            var actualObject = JObject.Parse(actual.Products.First().Documentation);
+            Assert.IsNotNull(actualObject);
+            actualObject["paths"].First().First().First().Children()["parameters"].Values()
+                .Any(c => c["name"].Value<string>() == "X-Version").Should().BeTrue();
+            actualObject["paths"].Last().Last().Last().Children()["parameters"].Values()
+                .Any(c => c["name"].Value<string>() == "X-Version").Should().BeTrue();
+        }
+
+        [Test, AutoData]
+        public void Then_The_SecurityScheme_For_Query_Is_Removed(Product product)
+        {
+            product.Documentation = testDocumentation;
+            var source = new GetProductsQueryResponse
+            {
+                Products = new List<Product> { product }
+            };
+            
+            var actual = (GetProductsApiResponse)source;
+
+            var actualObject = JObject.Parse(actual.Products.First().Documentation);
+            Assert.IsNotNull(actualObject);
+            actualObject["components"]["securitySchemes"].Children().Values()
+                .Any(c => c["name"].Value<string>() == "subscription-key").Should().BeFalse();
+        }
+        
+        
+        private string testDocumentation = @"{
+                                                ""openapi"": ""3.0.1"",
+                                                ""info"": {
+                                                    ""title"": ""Recruitment API"",
+                                                    ""description"": ""Create an advert on Find an apprenticeship using your existing systems."",
+                                                    ""version"": ""1""
+                                                },
+                                                ""servers"": [
+                                                    {
+                                                        ""url"": ""https://test/managevacancies""
+                                                    }
+                                                ],
+                                                ""paths"": {
+                                                    ""/accountlegalentities"": {
+                                                        ""get"": {
+                                                            ""tags"": [
+                                                                ""AccountLegalEntities""
+                                                            ],
+                                                            ""summary"": ""/accountlegalentities - GET"",
+                                                            ""operationId"": ""get-accountlegalentities"",
+                                                            ""parameters"": [
+                                                                {
+                                                                    ""name"": ""x-request-context-subscription-name"",
+                                                                    ""in"": ""header"",
+                                                                    ""schema"": {
+                                                                        ""type"": ""string""
+                                                                    }
+                                                                },
+                                                                {
+                                                                    ""name"": ""x-request-context-subscription-is-sandbox"",
+                                                                    ""in"": ""header"",
+                                                                    ""schema"": {
+                                                                        ""type"": ""string""
+                                                                    }
+                                                                }
+                                                            ],
+                                                            ""responses"": {
+                                                                ""200"": {
+                                                                    ""description"": ""Success""
+                                                                }
+                                                            }
+                                                        }
+                                                    },
+                                                    ""/referencedata/qualifications"": {
+                                                        ""get"": {
+                                                            ""tags"": [
+                                                                ""ReferenceData""
+                                                            ],
+                                                            ""summary"": ""/referencedata/qualifications - GET"",
+                                                            ""operationId"": ""get-referencedata-qualifications"",
+                                                            ""responses"": {
+                                                                ""200"": {
+                                                                    ""description"": ""Success""
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                },
+                                                ""components"": {
+                                                    ""schemas"": {},
+                                                    ""securitySchemes"": {
+                                                        ""apiKeyHeader"": {
+                                                            ""type"": ""apiKey"",
+                                                            ""name"": ""Ocp-Apim-Subscription-Key"",
+                                                            ""in"": ""header""
+                                                        },
+                                                        ""apiKeyQuery"": {
+                                                            ""type"": ""apiKey"",
+                                                            ""name"": ""subscription-key"",
+                                                            ""in"": ""query""
+                                                        }
+                                                    }
+                                                },
+                                                ""security"": [
+                                                    {
+                                                        ""apiKeyHeader"": []
+                                                    },
+                                                    {
+                                                        ""apiKeyQuery"": []
+                                                    }
+                                                ]
+                                            }";
     }
 }
--- src/SFA.DAS.Apim.Developer.Api/ApiResponses/GetProductsApiResponse.cs
+++ src/SFA.DAS.Apim.Developer.Api/ApiResponses/GetProductsApiResponse.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SFA.DAS.Apim.Developer.Application.AzureApimManagement.Queries.GetProducts;
 using SFA.DAS.Apim.Developer.Domain.Models;
 
@@ -21,6 +23,7 @@ namespace SFA.DAS.Apim.Developer.Api.ApiResponses
 
     public class GetProductsApiResponseItem
     {
+        
         public string Id { get ; set ; }
         public string Name { get; set; }
         public string DisplayName { get ; set ; }
@@ -31,15 +34,80 @@ namespace SFA.DAS.Apim.Developer.Api.ApiResponses
         public static implicit operator GetProductsApiResponseItem(Product source)
         {
             var isSandbox = source.Id.EndsWith("sandbox", StringComparison.InvariantCultureIgnoreCase);
-            
+
+            var documentationObject = PrepareOpenApiDocumentation(source);
+
             return new GetProductsApiResponseItem
             {
                 Id = source.Id,
                 Name = source.Name,
                 Description = source.Description,
                 DisplayName = source.DisplayName + (isSandbox ? " (Sandbox)" : " (Live)"),
-                Documentation = source.Documentation
+                Documentation = documentationObject
             };
+        }
+
+        private static string PrepareOpenApiDocumentation(Product source)
+        {
+            var notRequiredSecurityHeaders = new List<string>
+                { "x-request-context-subscription-name", "x-request-context-subscription-is-sandbox" };
+            var headerVersion = JObject.Parse(JsonConvert.SerializeObject(new HeaderVersion()));
+
+            var documentationObject = JObject.Parse(source.Documentation);
+            documentationObject["security"]?.FirstOrDefault(c => c["apiKeyQuery"] != null)?.Remove();
+            documentationObject["components"]?["securitySchemes"]?.Children().Values().FirstOrDefault(c => (c["name"] ?? "").Value<string>() == "subscription-key")?.Parent?.Remove();
+            if (documentationObject["paths"] == null)
+            {
+                return JsonConvert.SerializeObject(documentationObject);
+            }
+
+            foreach (var firstPath in documentationObject["paths"])
+            {
+                foreach (var secondPath in firstPath)
+                {
+                    foreach (var thirdPath in secondPath)
+                    {
+                        var children = thirdPath.Children()["parameters"].Values();
+                        if (children.Any())
+                        {
+                            children.FirstOrDefault()?.AddAfterSelf(headerVersion);
+                            foreach (var notRequiredHeader in notRequiredSecurityHeaders)
+                            {
+                                children.FirstOrDefault(c =>
+                                    c["name"].Value<string>()
+                                        .Equals(notRequiredHeader, StringComparison.CurrentCultureIgnoreCase))?.Remove();
+                            }
+                        }
+                        else
+                        {
+                            thirdPath.Children().Values().FirstOrDefault().AddAfterSelf(new JProperty("parameters",
+                                JArray.Parse(JsonConvert.SerializeObject(new List<HeaderVersion> { new HeaderVersion() }))));
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            return JsonConvert.SerializeObject(documentationObject);
+        }
+        private class HeaderVersion
+        {
+            [JsonProperty("name")]
+            public string Name = "X-Version";
+            [JsonProperty("in")]
+            public string In = "header";
+            [JsonProperty("schema")]
+            public SchemaVersion Schema = new SchemaVersion();
+            [JsonProperty("required")] 
+            public bool Required = true;
+            [JsonProperty("example")] 
+            public string Example = "1";
+            public class SchemaVersion
+            {
+                [JsonProperty("type")]
+                public string Type = "string";
+            }
         }
     }
 }

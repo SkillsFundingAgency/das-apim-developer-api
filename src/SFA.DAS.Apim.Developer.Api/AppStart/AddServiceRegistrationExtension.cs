76dc8619b850715b99e31d69cbf3e5f682abb02d
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-03-12 08:10:36 +0000

    FAI-3054 - Remove not needed registration code

--- src/SFA.DAS.Apim.Developer.Api/AppStart/AddServiceRegistrationExtension.cs
+++ src/SFA.DAS.Apim.Developer.Api/AppStart/AddServiceRegistrationExtension.cs
@@ -15,9 +15,6 @@ namespace SFA.DAS.Apim.Developer.Api.AppStart
 {
     public static class AddServiceRegistrationExtension
     {
-        private const int MaxRetries = 2;
-        private static readonly TimeSpan NetworkTimeout = TimeSpan.FromSeconds(1);
-        private static readonly TimeSpan Delay = TimeSpan.FromMilliseconds(100);
         public static void AddServiceRegistration(this IServiceCollection services)
         {
             services.AddHttpClient<IAzureApimManagementService, AzureApimManagementService>()
@@ -30,25 +27,7 @@ namespace SFA.DAS.Apim.Developer.Api.AppStart
             services.AddTransient<ISubscriptionService, SubscriptionService>();
             services.AddTransient<IUserService, UserService>();
             services.AddTransient<IProductService, ProductService>();
-            services.AddSingleton(new ChainedTokenCredential(
-                new ManagedIdentityCredential(options: new ManagedIdentityCredentialOptions()
-                {
-                    Retry = { NetworkTimeout = NetworkTimeout, MaxRetries = MaxRetries, Delay = Delay }
-                }),
-                new AzureCliCredential(options: new AzureCliCredentialOptions
-                {
-                    Retry = { NetworkTimeout = NetworkTimeout, MaxRetries = MaxRetries, Delay = Delay }
-                }),
-                new VisualStudioCredential(options: new VisualStudioCredentialOptions
-                {
-                    Retry = { NetworkTimeout = NetworkTimeout, MaxRetries = MaxRetries, Delay = Delay }
-                }),
-                new VisualStudioCodeCredential(options: new VisualStudioCodeCredentialOptions()
-                {
-                    Retry = { NetworkTimeout = NetworkTimeout, MaxRetries = MaxRetries, Delay = Delay }
-                }))
-            );
-
+            
             services.AddTransient<IApimUserRepository, ApimUserRepository>();
             services.AddTransient<IApimAuditRepository, ApimAuditRepository>();
             services.AddTransient<IApimSubscriptionAuditRepository, ApimSubscriptionAuditRepository>();
